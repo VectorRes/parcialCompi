3341c1fd613b02c46c7de3cc2cbcc25cdeea0e3f
VectorRes <atotoe2002@gmail.com>
2025-03-27 09:44:42 -0500

    Commit inicial

---
+++ src/test/java/com/compi/elitewings/EliteWingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compi.elitewings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliteWingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
