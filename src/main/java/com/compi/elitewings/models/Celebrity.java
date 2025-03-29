5906a70cb1220f1a8551479253f085c603e49c86
VectorRes <atotoe2002@gmail.com>
2025-03-29 12:03:12 -0500

    actualizacion del odelo Celebrity

--- src/main/java/com/compi/elitewings/models/Celebrity.java
+++ src/main/java/com/compi/elitewings/models/Celebrity.java
@@ -1,4 +1,29 @@
 package com.compi.elitewings.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity()
+@Table(name="celebrities")
+
 public class Celebrity {
+
+    @JsonIgnore
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String profession;
+
+    private double net_worth;
+
+    private boolean suspicious_activity;
 }
