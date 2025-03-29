d90004e831148b9e27fd0f212db11c95c8923456
Masiori <juanfegolo@gmail.com>
2025-03-29 15:46:07 -0500

    se completo el controlador de Celebrity

--- src/main/java/com/compi/elitewings/Controllers/CelebrityController.java
+++ src/main/java/com/compi/elitewings/Controllers/CelebrityController.java
@@ -1,10 +1,56 @@
 package com.compi.elitewings.Controllers;
 
 import com.compi.elitewings.models.Celebrity;
+import com.compi.elitewings.service.IServiceCelebrity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 
-
+import java.util.List;
+@RestController
+@RequestMapping("/celebrities")
 public class CelebrityController {
+    @Autowired
+    private IServiceCelebrity serviceCelebrity;
+
+    @GetMapping("/")
+    public List<Celebrity> index(){return this.serviceCelebrity.getCelebrities();}
+    @GetMapping("/celebrities/{id}")
+    public Celebrity getCelebrity(@PathVariable("id") long id){
+        return this.serviceCelebrity.getCelebrity(id).orElse(null);
+    }
+
+    @PostMapping("/celebrities")
+    public Celebrity createCelebrity(@RequestBody Celebrity celebrity){
+        this.serviceCelebrity.getCelebrities().add(celebrity);
+        return celebrity;
+    }
+
+    @DeleteMapping("/celebrities/{id}")
+    public String deleteCelebrity(@PathVariable("id") long id){
+        try {
+            this.serviceCelebrity.deleteCelebrity(id);
+            return "Celebrity with ID " + id + " deleted successfully.";
+        } catch (RuntimeException e) {
+            return e.getMessage();
+        }
+    }
+
+
+
+    @PutMapping("/celebrities/{id}")
+    public String updateCelebrity(@PathVariable("id") long id, @RequestBody Celebrity celebrity){
+        try {
+            celebrity.setId((int) id); // Aseguramos que el ID del objeto sea el mismo que el de la URL
+            this.serviceCelebrity.updateCelebrity(celebrity);
+            return "Celebrity with ID " + id + " updated successfully.";
+        } catch (RuntimeException e) {
+            return e.getMessage();
+        }
+    }
+
+
+
 
 
 }
