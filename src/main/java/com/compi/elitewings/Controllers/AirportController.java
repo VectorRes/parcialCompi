4f34183e35b70b884fb9a1b6d97868c219b14cd0
Masiori <juanfegolo@gmail.com>
2025-03-29 16:00:02 -0500

    se completo el controlador de Airport

--- src/main/java/com/compi/elitewings/Controllers/AirportController.java
+++ src/main/java/com/compi/elitewings/Controllers/AirportController.java
@@ -1,4 +1,21 @@
 package com.compi.elitewings.Controllers;
 
+import com.compi.elitewings.models.Airport;
+import com.compi.elitewings.service.IServiceAirport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping("/airports")
+
 public class AirportController {
+    @Autowired
+    private IServiceAirport serviceAirport;
+
+    @GetMapping("/airports")
+    public List<Airport> index(){return this.serviceAirport.getAirports();}
+    @GetMapping("/airports/{id}")
+    public Airport getAirport(@PathVariable("id") long id){
+        return this.serviceAirport.getAirport(id).orElse(null);
+    }
 }
